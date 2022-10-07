d33c41efe4eb55ed65837b1e617072342edd9b03
Vanessa <machado.vanessa32@gmail.com>
2022-10-06 21:40:52 -0300

    Page compartilhar ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { NewMomentsComponent } from './components/pages/new-moments/new-moments.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 
+
 const routes: Routes = [
   { path:'', component: HomeComponent },
   { path:'about', component: AboutComponent },
+  { path:'moments/new', component: NewMomentsComponent }, //
 ];
 
 @NgModule({
